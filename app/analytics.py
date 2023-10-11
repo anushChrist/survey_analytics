cdbe89eac16552f8b081d17f1144915c969fcb2b
anushChrist <anush.verma@msam.christuniversity.in>
2023-10-12 01:08:49 +0530

    working

--- app/analytics.py
+++ app/analytics.py
@@ -1,27 +1,168 @@
 # analytics.py
 import streamlit as st
 import pandas as pd
-
-
+from sklearn.model_selection import train_test_split as ttt
+from sklearn.linear_model import LinearRegression
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pymongo
 
 # Connect to MongoDB
 def anal(collection):
 
-    survey_data = pd.DataFrame(list(collection.find()))
+    df = pd.DataFrame(list(collection.find()))
     st.title("Analytics")
+
+    df = df.set_index("_id")
+
+    # COLUMN SPLITTING
+    num = [x for x in df if df[x].dtype == 'int64' and df[x].nunique() < 11]
+    cat = [x for x in df if df[x].dtype == 'object' and len(str(df[x].iloc[0])) < 30]
+    prf = ['gpa', 'stud_hb', 'stud_hr', 'attend']
+    ovhap = ['hap_rate', 'hap_fact']
+    hap = ['acad_impc', 'acad_press', 'acad_guilt', 'ch_guilt', 'gr_guilt', 'bal_contri', 'proc', 'wl_bal', 'extra_curr_hap', 'attd_impc']
 
     # Summary statistics
     st.header("Summary Statistics")
-    st.write(survey_data.describe())
+    st.write(df.describe())
 
     # Distribution of Age
     st.header("Age Distribution")
-    st.bar_chart(survey_data['age'].value_counts())
+    st.bar_chart(df['age'].value_counts())
 
     # Gender Distribution
     st.header("Gender Distribution")
-    st.bar_chart(survey_data['gen'].value_counts())
+    st.bar_chart(df['gen'].value_counts())
 
     # Academic Program Distribution
     st.header("Academic Program Distribution")
-    st.bar_chart(survey_data['acad_prg'].value_counts())
+    st.bar_chart(df['acad_prg'].value_counts())
+
+    # Happiness Factors
+    st.header("Happiness Factors")
+    f, a = plt.subplots(1, 1)
+    df[ovhap].groupby('hap_fact').mean().plot(kind='pie', subplots=True, ax=a)
+    st.pyplot(f)
+
+    # Year of study dist
+    st.header("Year of Study Distribution")
+    st.bar_chart(df['yo_stud'].value_counts())
+
+    # Select either Happiness or Performance
+    st.header("Select Happiness or Performance")
+    opt = st.selectbox("Select which metric to expand upon: ", ['Happiness', 'Performance'])
+
+    if opt == "Performance":
+        st.write("Performance attributes: ", prf)
+        genre = st.radio(
+            "Choose the type of plot",
+            ["KDE & Hist", "Groupby", "Scatter", "Violin & Boxplot"],
+            format_func=lambda x: "Density/Distribution" if x == "KDE & Hist" else ("Groupby various categorical attributes" if x == "Groupby" else "Outlier visualisation")
+        )
+        if genre == "KDE & Hist":
+            fig, ax = plt.subplots()
+            sns.kdeplot(df[prf].groupby('attend').mean(), ax=ax)
+            st.pyplot(fig)
+            #fig.savefig("kdeplot.png")
+
+        elif genre == "Groupby":
+            fig, ax = plt.subplots()
+            df[prf].groupby('attend').mean().plot(kind='barh', subplots=True)
+            st.pyplot(fig)
+            #fig.savefig("groupby.png")
+
+        elif genre == "Scatter":
+            fig, ax = plt.subplots()
+            sns.pairplot(df[prf], hue='attend')
+            st.pyplot(fig)
+            #fig.savefig("pairplot.png")
+
+        elif genre == "Violin & Boxplot":
+            fig, ax = plt.subplots()
+            sns.violinplot(df[prf].groupby('attend').mean())
+            st.pyplot(fig)
+            #fig.savefig("violinboxplot.png")
+    elif opt == "Happiness":
+        st.write("Happiness attributes: ", hap)
+        genre = st.radio(
+            "Choose the type of plot",
+            ["KDE & Hist", "Groupby", "Scatter", "Violin & Boxplot"],
+            format_func=lambda x: "Density/Distribution" if x == "KDE & Hist" else ("Groupby various categorical attributes" if x == "Groupby" else "Outlier visualisation")
+        )
+        if genre == "KDE & Hist":
+            fig, ax = plt.subplots()
+            sns.kdeplot(df[hap].groupby('attd_impc').mean(), ax=ax)
+            st.pyplot(fig)
+
+        elif genre == "Groupby":
+            fig, ax = plt.subplots()
+            df[hap].groupby('attd_impc').mean().plot(kind='barh', subplots=True)
+            st.pyplot(fig)
+
+        elif genre == "Scatter":
+            sns.pairplot(df[hap], hue='attd_impc')
+            st.pyplot()
+            
+
+        elif genre == "Violin & Boxplot":
+            fig, ax = plt.subplots()
+            sns.violinplot(df[hap].groupby('attd_impc').mean())
+            st.pyplot(fig)
+
+    # Correlation Analysis
+    st.header("Correlation Analysis")
+    optio = st.selectbox("Select the correlation matrix among: ", ['Performance', 'Happiness', 'All'])
+    if optio == 'Performance':
+        fig, ax = plt.subplots()
+        sns.heatmap(df[prf].drop('attend', axis=1).corr())
+        st.pyplot(fig)
+
+    elif optio == 'Happiness':
+        fig, ax = plt.subplots()
+        sns.heatmap(df[hap].drop('attd_impc', axis=1).corr())
+        st.pyplot(fig)
+
+    elif optio == 'All':
+        fig, ax = plt.subplots()
+        sns.heatmap(df[num].corr())
+        st.pyplot(fig)
+
+
+
+    # Regression Analysis
+    st.header("Regression analysis to check for dependency of happiness to performance")
+    st.write("Here we will aggregate all the performance and happiness metrics to continuous float. This will help us model a dependency relationship among them using Linear Regression")
+
+    x = pd.concat([df[hap].drop('attd_impc', axis=1), df[ovhap].drop('hap_fact', axis=1)], axis=1).mean(axis=1)
+    y = (2 * df[prf].gpa + 2 * df[prf].stud_hb + df[prf].stud_hr) / 5
+
+    # Convert NumPy arrays to Pandas Series
+    x = pd.Series(x)
+    y = pd.Series(y)
+
+    # Combine the Series into a DataFrame
+    data = pd.concat([x, y], axis=1)
+    st.dataframe(data)
+
+    slid = st.slider("Enter test size", min_value=0.0, max_value=1.0, value=0.2, step=0.05)
+
+    xtr, xte, ytr, yte = ttt(x.values.reshape(-1, 1), y.values.reshape(-1, 1), test_size=slid)
+
+    lr = LinearRegression()
+
+    lr.fit(xtr, ytr)
+    st.write(f'The R-square score is {lr.score(xte, yte)}')
+
+    st.write(f"The mean rating of performance is {float(lr.intercept_)} and if happiness increases by one unit, performance increases by {float(lr.coef_)} units.")
+
+    # Scatter plot of the regression line
+    fig, ax = plt.subplots()
+    sns.scatterplot(x=xte.reshape(1, -1)[0], y=yte.reshape(1, -1)[0], color='r')
+    st.pyplot(fig)
+
+    fig, ax = plt.subplots()
+    sns.scatterplot(x=xte.reshape(1, -1)[0], y=lr.predict(xte).reshape(1, -1)[0], color='b')
+
+
+
--- app/gen_data.py
+++ app/gen_data.py
@@ -1,71 +1,108 @@
-import streamlit as st
 import uuid
-import pymongo
 import random
+import streamlit as st
+import pandas as pd
+from pymongo import MongoClient
+import numpy as np
 
-# Connect to the MongoDB database
-client = pymongo.MongoClient("mongodb://localhost:27017")
-db = client["survey_db"]
-collection = db["survey_responses"]
+# Connect to MongoDB
+client = MongoClient("mongodb://localhost:27017")  # Replace with your MongoDB connection string
+db = client["survey_db"]  # Replace with your database name
+collection = db["survey_responses"]  # Replace with your collection name
 
-# Function to generate random data
-def generate_random_data():
-    age = random.randint(18, 35)
-    gender = random.choice(["Male", "Female", "Non-binary", "Prefer not to say"])
-    academic_program = random.choice(["Bachelors", "Masters", "Doctorate"])
-    year_of_study = random.choice(["1st year", "2nd Year", "3rd Year", "4th Year", "5th Year"])
-    gpa = random.randint(1, 5)
-    study_habits = random.randint(1, 5)
-    study_hours = random.randint(1, 10)  # Adjust the range as needed
-    class_attendance = random.choice(["Never", "Rarely", "Sometimes", "Often"])
-    happiness_rating = random.randint(1, 5)
-    happiness_factors = random.choice(["Family relationships", "Friendships", "Academic success", "Physical health", "Mental health", "Financial well-being", "Hobbies and interests", "Other"])
-    academic_impact = random.randint(1, 5)
-    academic_pressure = random.randint(1, 5)
-    academic_guilt = random.randint(1, 5)
-    balance_contribution = random.randint(1, 5)
-    procrastination = random.randint(1, 5)
-    extra_curricular_happiness = random.randint(1, 5)
-    work_life_balance = random.randint(1, 5)
-    cheating_guilt = random.randint(1, 5)
-    group_guilt = random.randint(1, 5)
-    attendance_impact = random.choice(["Positive", "Negative"])
-    additional_comments = ""
+def noise(p):
+    return random.uniform(-abs(p), p)  # Introduce random noise between -1 and 1
 
-    return {
-        "age": age,
-        "gen": gender,
-        "acad_prg": academic_program,
-        "yo_stud": year_of_study,
-        "gpa": gpa,
-        "stud_hb": study_habits,
-        "stud_hr": study_hours,
-        "attend": class_attendance,
-        "hap_rate": happiness_rating,
-        "hap_fact": happiness_factors,
-        "acad_impc": academic_impact,
-        "acad_press": academic_pressure,
-        "acad_guilt": academic_guilt,
-        "bal_contri": balance_contribution,
-        "proc": procrastination,
-        "wl_bal": work_life_balance,
-        "extra_curr_hap": extra_curricular_happiness,
-        "ch_guilt": cheating_guilt,
-        "gr_guilt": group_guilt,
-        "attd_impc": attendance_impact,
-        "add_comment": additional_comments
-    }
+# Function to generate sample data
+def generate_sample_data(num_samples):
+    data = []
 
-# Streamlit UI
-st.title("Generate Random Survey Data")
-number_of_samples = st.number_input("Number of samples to generate:", min_value=1, step=1)
-if st.button("Generate and Insert"):
-    for _ in range(number_of_samples):
+    for _ in range(num_samples):
         token = str(uuid.uuid4())
-        response_data = generate_random_data()
-        response_data["token"] = token
-        collection.insert_one(response_data)
-        st.write(f"Survey response with token {token} inserted successfully!")
+        
+        age = max(18, min(40, round(np.random.normal(24) + noise(3))))
+        
+        gender = random.choices(["Male", "Female", "Non-binary", "Prefer not to say"], weights=[0.4, 0.4, 0.1, 0.1])[0]  # Weighted random choice
+        academic_program = random.choices(["Bachelors", "Masters", "Doctorate"], weights=[0.6, 0.3, 0.1])[0]
+        year_of_study = random.choices(["1st year", "2nd Year", "3rd Year", "4th Year", "5th Year"], weights=[0.3, 0.3, 0.2, 0.1, 0.1])[0]
+        
+        # Generate GPA with a controlled non-uniform relationship with happiness
+        happiness_rating = max(1, min(5, round(np.random.normal(3, 1) + noise(1))))  # Clamp and round to the nearest integer
+       
+        balance_contribution = max(1, min(5, round(np.random.normal(3, 1) + noise(1))))
+        extra_curricular_happiness = max(1, min(5, round(np.random.normal(3, 1) + noise(2))))        
+        procrastination = max(1, min(5, round(np.random.normal(4, 1) + noise(1))))
 
-# Close the MongoDB connection
-client.close()
+        work_life_balance =  max(1, min(5, round(np.mean(np.random.normal(2, 1) + noise(1) + balance_contribution - (2*procrastination) + extra_curricular_happiness))))
+
+        study_habits = max(1, min(5, round(np.mean(np.random.normal(3, 1) + noise(2) + work_life_balance - procrastination))))
+        
+
+        study_hours = max(1, min(10, round(study_habits + noise(3))))  
+        scaled_study_hours = (study_hours / 2)
+
+        class_attendance = random.choices(["Never", "Rarely", "Sometimes", "Often"], weights=[0.2, 0.3, 0.3, 0.2])[0]  # Weighted random choice
+        attendance_mapping = {"Never": 4, "Rarely": 3, "Sometimes": 2,"Often": 1}
+        num_class_attendance = attendance_mapping[class_attendance] / 4 * 5
+
+        
+        
+        academic_impact = max(1, min(5, round(np.random.normal(3, 1) + noise(1))))  # Normal distribution with a mean of 3
+
+        academic_pressure = max(1, min(5, round(academic_impact + noise(1.5))))  # Normal distribution with a mean of 3
+        
+        academic_guilt = max(1, min(5, round(academic_impact + noise(1))))
+        happiness_factors = random.choices(["Family relationships", "Friendships", "Academic success", "Physical health", "Mental health", "Financial well-being", "Hobbies and interests", "Other"], weights=[0.1, 0.1, 0.2, 0.1, 0.2, 0.1, 0.1, 0.1])[0]
+        
+        
+        
+
+        gpa = max(1, min(5, round(np.mean(happiness_rating + study_habits + scaled_study_hours + num_class_attendance + academic_pressure + academic_impact - (3*procrastination)))))
+        
+        cheating_guilt = max(1, min(5, round(np.mean(np.random.normal(3, 1) + academic_guilt + noise(2)))))
+        
+        group_guilt = random.choices([1, 2, 3, 4, 5], weights=[0.3, 0.1, 0.1, 0.2, 0.3])[0]  # Weighted random choice
+        
+        attendance_impact = random.choices(["Positive", "Negative"], weights=[0.7, 0.3])[0]  # Weighted random choice
+        
+        additional_comments = ""
+
+        response = {
+            "token": token,
+            "age": age,
+            "gen": gender,
+            "acad_prg": academic_program,
+            "yo_stud": year_of_study,
+            "gpa": gpa,
+            "stud_hb": study_habits,
+            "stud_hr": study_hours,
+            "attend": class_attendance,
+            "hap_rate": happiness_rating,
+            "hap_fact": happiness_factors,
+            "acad_impc": academic_impact,
+            "acad_press": academic_pressure,
+            "acad_guilt": academic_guilt,
+            "bal_contri": balance_contribution,
+            "proc": procrastination,
+            "wl_bal": work_life_balance,
+            "extra_curr_hap": extra_curricular_happiness,
+            "ch_guilt": cheating_guilt,
+            "gr_guilt": group_guilt,
+            "attd_impc": attendance_impact,
+            "add_comment": additional_comments
+        }
+
+        data.append(response)
+
+    return data
+
+# Streamlit user interface
+st.title("Student Happiness and Academic Performance Survey Data Generator")
+num_samples = st.number_input("Enter the number of samples you want to generate:", min_value=1, value=100)
+
+if st.button("Generate and Insert Data"):
+    sample_data = generate_sample_data(num_samples)
+    collection.insert_many(sample_data)
+    st.success(f"Inserted {num_samples} sample records into the MongoDB database.")
+
+st.write("This app generates sample data for a student survey with non-uniform, right-skewed distributions.")
